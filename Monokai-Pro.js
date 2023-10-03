ccb0efb7b0e72d4f6034c666fbcfc2446f3b3368
Joey Paak <99041363+joeypaak@users.noreply.github.com>
2023-10-03 14:11:02 +0900

    Add Monokai-Pro.js

---
+++ Monokai-Pro.js
@@ -0,0 +1,21 @@
+t.prefs_.set("color-palette-overrides", [
+  "#2D2A2E",
+  "#FF6188",
+  "#A9DC76",
+  "#FFD866",
+  "#FC9867",
+  "#AB9DF2",
+  "#78DCE8",
+  "#FCFCFA",
+  "#727072",
+  "#FF6188",
+  "#A9DC76",
+  "#FFD866",
+  "#FC9867",
+  "#AB9DF2",
+  "#78DCE8",
+  "#FCFCFA",
+]);
+t.prefs_.set("foreground-color", "#FCFCFA");
+t.prefs_.set("background-color", "#2D2A2E");
+t.prefs_.set("cursor-color", "rgba(252,252,250,0.5)");
